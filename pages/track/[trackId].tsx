4d15f9a10af7a7241752cbb1a3843eaf2e794125
Martinus123S <martinussitinjak225@gmail.com>
2021-10-15 12:53:47 +0700

    delete track

--- pages/track/[trackId].tsx
+++ pages/track/[trackId].tsx
@@ -1,4 +1,17 @@
-import { Box, Text, Stack, Image, SimpleGrid } from "@chakra-ui/react";
+import {
+  Box,
+  Text,
+  Stack,
+  Image,
+  SimpleGrid,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
 import { Button } from "@chakra-ui/button";
 import { useRouter } from "next/dist/client/router";
 import { useParams } from "react-router";
@@ -24,7 +37,8 @@ export interface IPLaylistTrack {
 const Track: React.FC = () => {
   const router = useRouter();
   const { trackId } = router.query;
-
+  const [isOpen, setIsopen] = useState<boolean>(false);
+  const [id, setId] = useState("");
   const [playlistTrack, setPlaylistTrack] = useState<IPLaylistTrack[]>([]);
   console.log(playlistTrack);
   useEffect(() => {
@@ -35,43 +49,55 @@ const Track: React.FC = () => {
     PlaylistTrackAPI();
   }, [trackId]);
 
+  const deleteOneTrack = (id: string) => {
+    const spreadTheData = [...playlistTrack];
+
+    const newData = spreadTheData?.filter((data) => {
+      return data.track.id !== id;
+    });
+
+    setPlaylistTrack(newData);
+    setIsopen(false);
+  };
   return (
-    <Box margin="1rem">
-      <Button
-        colorScheme="blue"
-        borderRadius="1rem"
-        onClick={() => {
-          router.push("/profile");
-        }}
-      >
-        Back
-      </Button>
-      <Text>Your Playlist Track</Text>
-      <SimpleGrid columns={[2, null, 5]} gap={5}>
-        {playlistTrack.map((val, index) => {
-          return (
-            <Box
-              key={index}
-              maxW="lg"
-              display="flex"
-              flexDirection="column"
-              borderWidth="1px"
-              borderRadius="lg"
-              borderColor="white"
-              overflow="hidden"
-              cursor="pointer"
-              _hover={{
-                borderColor: "blue",
-              }}
-            >
-              <Image
-                width={"100%"}
-                height={"10rem"}
+    <>
+      <Box margin="1rem" p="1rem">
+        <Button
+          colorScheme="blue"
+          borderRadius="1rem"
+          onClick={() => {
+            router.push("/profile");
+          }}
+        >
+          Back
+        </Button>
+        <Text marginTop="1rem" textColor="white" fontSize="1.5rem">
+          Your Playlist Track
+        </Text>
+        <SimpleGrid columns={[2, null, 5]} gap={5}>
+          {playlistTrack.map((val, index) => {
+            return (
+              <Box
                 key={index}
-                src={val.track.album.images[0].url}
-              />
-              <Box p="3">
-                <Box display="flex" alignItems="flex-start">
+                width="100%"
+                display="flex"
+                flexDirection="column"
+                borderWidth="1px"
+                borderRadius="lg"
+                borderColor="white"
+                overflow="hidden"
+                cursor="pointer"
+                _hover={{
+                  borderColor: "blue",
+                }}
+              >
+                <Image
+                  width={"100%"}
+                  height={"10rem"}
+                  key={index}
+                  src={val.track.album.images[0].url}
+                />
+                <Box p="3" display="flex" flexDirection="column">
                   <Box
                     color="gray.500"
                     fontWeight="semibold"
@@ -81,13 +107,44 @@ const Track: React.FC = () => {
                   >
                     {val.track.name}
                   </Box>
+                  <Button
+                    colorScheme="red"
+                    onClick={() => {
+                      setId(val.track.id);
+                      setIsopen(true);
+                    }}
+                  >
+                    Delete
+                  </Button>
                 </Box>
               </Box>
-            </Box>
-          );
-        })}
-      </SimpleGrid>
-    </Box>
+            );
+          })}
+        </SimpleGrid>
+      </Box>
+      <Modal isOpen={isOpen} onClose={() => setIsopen(false)} size="4xl">
+        <ModalOverlay />
+        <ModalContent color="white" bgColor="white" fontSize="2rem">
+          <ModalHeader fontSize="3rem">Delete this from playlist</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody fontSize="2rem">Are you sure to delete this?</ModalBody>
+
+          <ModalFooter fontSize="2rem">
+            <Button onClick={() => setIsopen(false)} colorScheme="blue" mr={3}>
+              Close
+            </Button>
+            <Button
+              color="white"
+              backgroundColor="red.500"
+              _hover={{ backgroundColor: "red.400" }}
+              onClick={() => deleteOneTrack(id)}
+            >
+              Delete
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
